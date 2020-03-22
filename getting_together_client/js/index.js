de20caae650e51bb1743833471d4a22cec7bfbfc
Eric J Wilhite <EricJWilhite@gmail.com>
2020-03-22 15:53:49 -0700

    refactor index.js

--- getting_together_client/js/index.js
+++ getting_together_client/js/index.js
@@ -1,38 +1,5 @@
 class EventAPI {
     static getEvents() {
-        return fetch(`${EventAPI.base_url}/events`).then( res => { debugger; } )
+        return fetch(`${EventAPI.base_url}/events`).then(res => {return res.json()} )
     }
 }
-
-class Event {
-    constructor({ title, description, service, date, time }) {
-        this.title = title, 
-        this.description = description,
-        this.service = service, 
-        this.date = date, 
-        this.time = time
-    }
-
-    render() {
-        EventAPI.getEvents().then(events => {
-            events.map(eventAttr => {
-                console.log(eventAttr)
-            })
-        })
-    }
-}
-
-Event.all = []
-
-class EventsPage {
-    constructor(events) {
-        this.events = events
-    }
-
-    render () {
-        return `<p>${this.events}</p>`
-    }
-
-}
-
-EventAPI.base_url = "http://localhost:3000"
